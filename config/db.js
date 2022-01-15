884e9fe958ebc063bd8ffebad8053265ab760ec7
Agamyrat <agamyrat.chariyev@gmail.com>
2022-01-15 09:53:50 +0500

    db config added

---
+++ config/db.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Sequelize } = require('sequelize');
+const { DB_SERVER, DB_NAME, DB_USER_NAME, DB_PASSWORD, DB_PORT } = process.env;
+
+if (!DB_SERVER || !DB_NAME || !DB_USER_NAME || !DB_PASSWORD || !DB_PORT) {
+    new Error(
+        'Error occurred in db settings configurations. Some environments missing.'
+    );
+}
+
+module.exports = new Sequelize(DB_NAME, DB_USER_NAME, DB_PASSWORD, {
+    host: DB_SERVER,
+    dialect: 'mssql',
+    port: DB_PORT,
+    dialectOptions: {
+        options: {
+            requestTimeout: 3000000,
+            useUTC: true,
+            dateFirst: 1,
+        },
+    },
+
+    define: {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        freezeTableName: true,
+        underscored: true,
+        timestamps: false,
+    },
+});
