c70a167cb4ff3a5eabdd4c4bdd5bb8f8ff9c74ad
Agamyrat Chariyev <agamyrat.chariyev@gmail.com>
2022-04-04 10:36:09 +0500

    ssl removed

--- server.js
+++ server.js
@@ -9,19 +9,29 @@ dotenv.config({ path: path.join(__dirname, 'config', '.env') });
 const app = require('./app');
 const PORT = process.env.PORT;
 
-const sslServer = https.createServer(
-    {
-        key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-    },
-    app
-);
+// const sslServer = https.createServer(
+//     {
+//         key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//         cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+//     },
+//     app
+// );
 
-sslServer.listen(PORT, (err) => {
+// sslServer.listen(PORT, (err) => {
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(
+//         `App  is running on port ${PORT}, NODE_ENV:${process.env?.NODE_ENV}`
+//     );
+// });
+
+app.listen(PORT, (err) => {
     if (err) {
         console.error(err);
+    } else {
+        console.log(
+            `\n----------------------------\nApp is running on port ${PORT}\n----------------------------\n\n`
+        );
     }
-    console.log(
-        `App  is running on port ${PORT}, NODE_ENV:${process.env?.NODE_ENV}`
-    );
 });
